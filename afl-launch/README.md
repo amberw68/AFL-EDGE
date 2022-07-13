be76aed98942ced4e93e7767b0b2f05fb370c884
Yifan <yw03021103@gmail.com>
2022-07-12 21:23:55 -0400

    tool for running afl instances concurrently

---
+++ afl-launch/README.md
@@ -0,0 +1,110 @@
+afl-launch
+=======
+
+## About
+
+`afl-launch` is a simple program to spawn afl fuzzing instances from the
+command line. It provides no compelling features; it is simply my version of
+this tool.
+
+```
+Usage of afl-launch:
+  -XXX
+        [HACK] substitute XXX in the target args with an 8 char random string [HACK]
+  -f string
+        Filename template (substituted and passed via -f)
+  -i string
+        afl-fuzz -i option (input location)
+  -m string
+        afl-fuzz -m option (memory limit), 'none' for no limit (defaults to afl)
+  -n int
+        Number of instances to launch (default 1)
+  -name string
+        Base name for instances. Fuzzers will work in <output>/<BASE>-[M|S]<N>
+  -no-master
+        Launch all instances with -S
+  -o string
+        afl-fuzz -o option (output location)
+  -t string
+        afl-fuzz -t option (timeout)
+  -x string
+        afl-fuzz -x option (extras location)
+```
+
+The launcher DOES NOT CHECK if the `afl-fuzz` instance errored out. Before
+starting a multiple launch, you should start `afl-fuzz` once manually with your
+desired `-i` `-o` `-x` (etc) options to make sure everything works.
+
+If you don't supply a base name, the launcher will pick a random one.
+
+Example:
+```
+./afl-launch -i ~/testcases/pdf -o ~/fuzzing/pdf -n 4  -- pdftoppm @@
+```
+
+A note on the `-f` flag - the idea is that you pass a template like
+`/dev/shm/whatever.xml` and the launcher will substitute it as `-f
+/dev/shm/<BASENAME>-S12.xml` when it invokes `afl-fuzz`. This is so that you can
+have AFL create testcase files on a ramdisk, and avoid stressing your disks.
+Queue entries and crashes are still saved as usual in the location specified
+by `-o`. Don't be an idiot like me and run everything on a ramdisk.
+
+Another note about ttys - this tool just spawns all the processes and then
+exits. If you want them to stay running unattended then the easiest and (IMHO)
+best way is just to run it inside a `screen` session (`man screen`).
+
+### -XXX
+
+There is a hacky option that can be used for a few things. If you pass -XXX
+then the literal string `XXX` anywhere in the target command (after the `--`
+in the command line) will be replaced with a random 8 character string. I use
+this for targets that require a `-o` flag for output filename, like
+`stupidprogram -i @@ -out /dev/shm/XXX.jpg`.
+
+### They launched.. now what?
+
+Use `afl-whatsup <LOCATION>` with the same location you used for -o to get the
+afl-fuzz summary output. For bonus points, be a unix nerd and do like `watch
+-n 60 afl-whatsup -s ~/fuzzing/targetname`
+
+This is what that looks like:
+```
+Every 60.0s: afl-whatsup -s ~/fuzzing/targetname Sun Jun  7 10:40:36 2015
+
+status check tool for afl-fuzz by <lcamtuf@google.com>
+
+Summary stats
+=============
+
+       Fuzzers alive : 40
+      Total run time : 161 days, 22 hours
+         Total execs : 4513 million
+    Cumulative speed : 12904 execs/sec
+       Pending paths : 75 faves, 29250 total
+  Pending per fuzzer : 1 faves, 731 total (on average)
+       Crashes found : 9806 locally unique
+```
+
+## Installation
+
+You should follow the [instructions](https://golang.org/doc/install) to
+install Go, if you haven't already done so.
+
+Download, build and install `afl-launch`:
+```bash
+$ go get -u github.com/bnagy/afl-launch
+```
+
+## TODO
+
+Nothing on the list. Open an issue if you want something.
+
+## Contributing
+
+* Fork and send a pull request
+* Report issues
+
+## License & Acknowledgements
+
+BSD style, see LICENSE file for details.
+
---
+++ afl-launch/main_no_@@.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const MAXFUZZERS = 256
+
+
+var (
+	flagAFL   = flag.String("afl", "", "afl-fuzz option (afl)")
+	flagNoMaster = flag.Bool("no-master", false, "Launch all instances with -S")
+	flagNum      = flag.Int("n", 1, "Number of instances to launch")
+	flagName     = flag.String("name", "", "Base name for instances. Fuzzers will work in <output>/<BASE>-[M|S]<N>")
+	flagTimeout  = flag.String("t", "", "afl-fuzz -t option (timeout)") // afl -t option supports a + suffix
+	flagMem      = flag.String("m", "", "afl-fuzz -m option (memory limit)")
+	flagInput    = flag.String("i", "", "afl-fuzz -i option (input location)")
+	flagExtras   = flag.String("x", "", "afl-fuzz -x option (extras location)")
+	flagOutput   = flag.String("o", "", "afl-fuzz -o option (output location)")
+	flagFile     = flag.String("f", "", "Filename template (substituted and passed via -f)")
+	flagXXX      = flag.Bool("XXX", false, "[HACK] substitute XXX in the target args with an 8 char random string [HACK]")
+
+	subRegex = regexp.MustCompile("XXX")
+)
+
+func randomName(n int) (result string) {
+	buf := make([]byte, n)
+	_, err := io.ReadFull(rand.Reader, buf)
+	if err != nil {
+		panic(err)
+	}
+	for _, b := range buf {
+		result += string(b%26 + 0x61)
+	}
+	return
+}
+
+func spawn(fuzzerName string, args []string) {
+
+	// if the user wants to use a special location for the testfiles ( like a
+	// ramdisk ) then they can provide any filename /path/to/whatever.xxx and
+	// we'll sub out 'whatever' for the name of this fuzzer and keep the base
+	// and the extension.
+	if len(*flagFile) > 0 {
+		base, _ := path.Split(*flagFile)
+		ext := path.Ext(*flagFile)
+		args = append(args, "-f", path.Join(base, fuzzerName+ext))
+	}
+
+	// Create a logfile for afl's stdout. Truncates any existing logfile.
+	fuzzerDir := path.Join(*flagOutput, fuzzerName)
+	err := os.MkdirAll(fuzzerDir, 0777)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	fd, err := os.Create(path.Join(fuzzerDir, "afl-launch.log"))
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	defer fd.Close()
+
+	args = append(args, "--")
+	if *flagXXX {
+		progArgs := make([]string, len(flag.Args()))
+		copy(progArgs, flag.Args())
+		for i, elem := range progArgs {
+			if subRegex.MatchString(elem) {
+				progArgs[i] = subRegex.ReplaceAllString(elem, randomName(8))
+			}
+		}
+		args = append(args, progArgs...)
+	} else {
+		args = append(args, flag.Args()...)
+	}
+
+	cmd := exec.Command(*flagAFL, args...)
+	cmd.Stdout = fd
+	err = cmd.Start()
+	if err != nil {
+		// If this fails to start it will be OS issues like no swap or rlimit
+		// or something, so it's not something we can handle gracefully. It's
+		// NOT the same as the afl-fuzz process exiting because the args are
+		// incorrect.
+		log.Fatalf(err.Error())
+	}
+	cmd.Process.Release()
+	log.Printf("%s %s\n", *flagAFL, strings.Join(args, " "))
+}
+
+func main() {
+
+	flag.Parse()
+	if len(flag.Args()) < 1 {
+		log.Fatalf("no command to fuzz, eg: targetname @@")
+	}
+
+	if *flagNum > MAXFUZZERS {
+		log.Fatalf("too many fuzzers: %d", *flagNum)
+	}
+	// sanity for name
+	if len(*flagName) > 32 {
+		log.Fatalf("base name too long (%d), must be <= 32", len(*flagName))
+	}
+
+	// collect the proxy args for afl-fuzz
+	baseArgs := []string{}
+	for _, v := range []string{"t", "m", "i", "x", "o"} {
+		f := flag.Lookup(v)
+		if f != nil && f.Value.String() != f.DefValue {
+			baseArgs = append(baseArgs, "-"+v, f.Value.String())
+		}
+	}
+
+	baseName := *flagName
+	if len(baseName) == 0 {
+		baseName = randomName(5)
+	}
+
+	// first instance is a master unless indicated otherwise
+	if *flagNoMaster {
+		name := baseName + "_" + "S" + "_" + "0"
+		spawn(name, append(baseArgs, "-S", name))
+	} else {
+		name := baseName + "_" + "M" + "_" + "0"
+		spawn(name, append(baseArgs, "-M", name))
+	}
+
+	// launch the rest
+	for i := 1; i < *flagNum; i++ {
+		name := baseName + "_" + "S" + "_" + strconv.Itoa(i)
+		spawn(name, append(baseArgs, "-S", name))
+	}
+}
